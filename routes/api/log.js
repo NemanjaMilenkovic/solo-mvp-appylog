ae6d6f0cb54958265d7ea7d09bdcfc94b0046910
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-02-11 23:01:37 +0900

    added frontend

--- routes/api/log.js
+++ routes/api/log.js
@@ -5,6 +5,7 @@ const db = require("../../db/");
 router.get("/", (req, res) => {
   db.select()
     .from("logs")
+    .orderBy("id")
     .then(data => res.send(data));
 });
 
@@ -14,6 +15,39 @@ router.post("/", (req, res) => {
     .into("logs")
     .then(data => {
       res.send(data);
+    });
+});
+
+router.patch("/:id", (req, res) => {
+  db.table("logs")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .returning("*")
+    .then(data => {
+      res.send(data);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("logs")
+    .where({ id: req.params.id })
+    .select()
+    .then(data => res.send(data));
+});
+
+router.delete("/:id", (req, res) => {
+  const toDelete = {};
+  db("logs")
+    .where({ id: req.params.id })
+    .select()
+    .then(data => (toDelete.logDeleted = data));
+
+  db("logs")
+    .where({ id: req.params.id })
+    .del()
+    .then(data => {
+      toDelete.deleted = true;
+      res.send(toDelete);
     });
 });
 

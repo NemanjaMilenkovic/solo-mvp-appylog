fc3617f699d8c7a9b2c2e6e6c52a7e8911476a3a
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-02-12 02:12:25 +0900

    refactoring for Heroku deploy

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use("/api", apiRoute);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

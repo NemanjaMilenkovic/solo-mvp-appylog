1441cf52741c9b425257fea48d505992299cdbb4
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-02-11 21:12:43 +0900

    added a POST request

--- routes/api/log.js
+++ routes/api/log.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db/");
 
-router.get("/", function(req, res) {
+router.get("/", (req, res) => {
   db.select()
     .from("logs")
     .then(data => res.send(data));
+});
+
+router.post("/", (req, res) => {
+  db.insert(req.body)
+    .returning("*")
+    .into("logs")
+    .then(data => {
+      res.send(data);
+    });
 });
 
 module.exports = router;

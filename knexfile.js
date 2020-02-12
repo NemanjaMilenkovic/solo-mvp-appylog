19c52bd03697075d69feba714a38516b6312f9a9
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-02-12 14:56:42 +0900

    updating env variables

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,15 @@
-const knex = require("knex");
-
-require("dotenv").config();
-
-const db = knex({
-  client: "pg",
-  connection:
-    process.env.DATABASE_URL ||
-    `postgres://${process.env.USER}@127.0.0.1:5432/appylog_mvp`,
-  searchPath: "public"
-});
-
-module.exports = db;
+module.exports = {
+  client: "postgresql",
+  connection: {
+    database: "appylog_mvp",
+    user: "postgres",
+    password: "password"
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
